69391b91589eafce3578154ef22ddd5ec298cda5
itsRoze <elewis9989@gmail.com>
2024-03-18 16:19:48 -0400

    show errors in bufferlien

--- .config/nvim/lua/roze/plugins/bufferline.lua
+++ .config/nvim/lua/roze/plugins/bufferline.lua
@@ -6,6 +6,26 @@ return {
     require("bufferline").setup({
       options = {
         separator_style = { "", "" },
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(_, _, diagnostics)
+          local result = {}
+          local symbols = { error = " ", warning = " ", info = " " }
+          for name, count in pairs(diagnostics) do
+            if symbols[name] and count > 0 then
+              table.insert(result, symbols[name] .. count)
+            end
+          end
+          local res = table.concat(result, " ")
+          return #res > 0 and res or ""
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
       },
     })
   end,
