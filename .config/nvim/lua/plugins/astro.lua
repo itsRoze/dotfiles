65cbc226c0c4e93e09b577498e08ed0b1b248ce5
itsRoze <elewis9989@gmail.com>
2024-04-30 16:00:32 -0400

    astro

--- .config/nvim/lua/plugins/astro.lua
+++ .config/nvim/lua/plugins/astro.lua
@@ -11,9 +11,43 @@ return {
       formatters_by_ft = {
         astro = { "prettier" },
       },
+      formatters = {
+        prettier = {
+          condition = function(ctx)
+            return vim.fs.find({
+              ".prettierrc",
+              ".prettierrc.json",
+              ".prettierrc.yml",
+              ".prettierrc.yaml",
+              ".prettierrc.json5",
+              ".prettierrc.js",
+              "prettier.config.js",
+              "prettier.config.ts",
+              ".prettierrc.mjs",
+              "prettier.config.mjs",
+              ".prettierrc.cjs",
+              "prettier.config.cjs",
+              ".prettierrc.toml",
+            }, { path = ctx.filename, upward = true })[1]
+          end,
+
+          prepend_args = function(_, ctx)
+            local filetype = require("plenary.filetype").detect_from_extension(ctx.filename)
+
+            if filetype ~= "astro" then
+              return {}
+            end
+
+            return {
+              "--stdin-filepath",
+              ctx.bufname,
+              "--plugin=prettier-plugin-astro",
+            }
+          end,
+        },
+      },
     },
   },
-
   {
     "nvim-tree/nvim-web-devicons",
     opts = {
