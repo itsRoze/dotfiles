cc97cd97505e63bb6a34bb3c5f76d0abee70b71e
itsRoze <elewis9989@gmail.com>
2024-09-05 12:19:01 -0400

    try gruvbox to see if everforest theme is causing issue, remove laravel and sql

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,28 @@
 return {
+  -- {
+  --   "neanias/everforest-nvim",
+  --   version = false,
+  --   lazy = false,
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- Optional; default configuration will be used if setup isn't called.
+  --   config = function()
+  --     require("everforest").setup({
+  --       background = "soft",
+  --       transparent_background_level = 1,
+  --       transparent_background = true,
+  --     })
+  --   end,
+  -- },
   {
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    -- Optional; default configuration will be used if setup isn't called.
-    config = function()
-      require("everforest").setup({
-        background = "soft",
-        transparent_background_level = 1,
-        transparent_background = true,
-      })
-    end,
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "gruvbox",
     },
   },
 }
