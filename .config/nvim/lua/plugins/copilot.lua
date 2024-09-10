c34ace682ac0a6de50ca0c08b64da3432f627b72
itsRoze <elewis9989@gmail.com>
2024-09-10 15:37:59 -0400

    switch to codeium

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,17 @@
-return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      auto_refresh = true,
-      keymap = {
-        accept = "<C-l>",
-        dismiss = "<C-e>",
-        next = "<c-[>",
-        prev = "<c-]>",
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   "zbirenbaum/copilot.lua",
+--   opts = {
+--     suggestion = {
+--       enabled = true,
+--       auto_trigger = true,
+--       auto_refresh = true,
+--       keymap = {
+--         accept = "<C-l>",
+--         dismiss = "<C-e>",
+--         next = "<c-[>",
+--         prev = "<c-]>",
+--       },
+--     },
+--   },
+-- }
